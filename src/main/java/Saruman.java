a737f6ae2209d6ab09dc0da37e5b0a7e0c7edb81
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-28 21:46:39 +0400

    Buratino task added

---
+++ src/main/java/Saruman.java
@@ -0,0 +1,8 @@
+public class Saruman {
+    public static void main(String[] args) {
+     int people = 125;
+     int orcs = 70;
+     int warriors = (people + orcs);
+     System.out.println("Общее количество пехотинцев в союзе: " + warriors);
+    }
+}

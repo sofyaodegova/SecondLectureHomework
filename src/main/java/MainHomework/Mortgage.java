bdd00165a9fcc6fe9c5bf437922f6da85a8f1b5a
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-28 22:19:04 +0400

    MainHomework package added; Age of Universe task added;

---
+++ src/main/java/MainHomework/Mortgage.java
@@ -0,0 +1,10 @@
+package MainHomework;
+
+public class Mortgage {
+    public static void main(String[] args) {
+        long flatCosts = 3000000;
+        double firstPayment = 0.15;
+        double money = (flatCosts * firstPayment);
+        System.out.println("Первоначальный взнос по ипотеке " + money + " рублей");
+    }
+}

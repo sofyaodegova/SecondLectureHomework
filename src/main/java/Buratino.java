a3493a8fce4574c366f70878b5ebe229341df484
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-28 21:36:10 +0400

    Buratino task added

---
+++ src/main/java/Buratino.java
@@ -0,0 +1,9 @@
+public class Buratino {
+    public static void main(String[] args) {
+        int apples = 10;
+        int steal = 7;
+        int total = (apples - steal);
+        System.out.println("У Буратино осталось " + total + " яблок");
+
+    }
+}

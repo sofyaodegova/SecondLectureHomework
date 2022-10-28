523470bbd1770ea4a8cb7bd50bdfd4b292534ac0
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-28 22:04:10 +0400

    Glass task added

---
+++ src/main/java/Glass.java
@@ -0,0 +1,9 @@
+public class Glass {
+    public static void main(String[] args) {
+        int v = 200;
+        double full = 0.6;
+        double water = (v * full);
+        System.out.println("Всего в стакане " + water + " миллилитров воды");
+
+    }
+}

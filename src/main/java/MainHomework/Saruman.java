39ec974eea68f502081b38a5ac8d88d4c0e5bf0f
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-30 20:23:11 +0400

    Fixed

--- src/main/java/MainHomework/Saruman.java
+++ src/main/java/MainHomework/Saruman.java
@@ -2,9 +2,9 @@ package MainHomework;
 
 public class Saruman {
     public static void main(String[] args) {
-     int people = 125;
-     int orcs = 70;
-     int warriors = (people + orcs);
-     System.out.println("Общее количество пехотинцев в союзе: " + warriors);
+        int people = 125;
+        int orcs = 70;
+        int warriors = (people + orcs);
+        System.out.println("Общее количество пехотинцев в союзе: " + warriors);
     }
 }
